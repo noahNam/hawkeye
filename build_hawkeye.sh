b7d47fb2646e55ed8833c893b82314be0459febc
Noah Nam <noah@apartalk.com>
2021-11-15 16:28:02 +0900

    빌드파일 requests 패키지 추가

--- build_hawkeye.sh
+++ build_hawkeye.sh
@@ -1,2 +1,2 @@
 cp ./deploy/lambda_function_hawkeye.py ./lambda_function.py
-zip -r ./deploy/lambda_function_hawkeye.zip ./psycopg2 ./lambda_function.py
+zip -r ./deploy/lambda_function_hawkeye.zip ./psycopg2 ./requests ./lambda_function.py
